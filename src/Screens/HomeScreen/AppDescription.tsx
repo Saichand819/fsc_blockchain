1478bd67e1c74a4f6e1aa094d3354f04b6a460f5
Saichand819 <saichandgupta819@gmail.com>
2022-12-03 16:52:23 +0530

    Description Component in Home Page

---
+++ src/Screens/HomeScreen/AppDescription.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const SAppDescription = styled.div `
+    height: 150px;
+`
+
+function AppDescription() {
+  return (
+    <SAppDescription>AppDescription</SAppDescription>
+  )
+}
+
+export default AppDescription
